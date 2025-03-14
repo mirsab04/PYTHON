0af4ee58314dfdd7388fbfb734eab18e0b53f179
HAFIZ MUHAMMAD MIRSAB ALAM <mirsabalam989@gmail.com>
2025-03-14 13:25:49 +0500

    Add files via upload

---
+++ CHECK ALPHA AND DIGIT METHOD.py
@@ -0,0 +1,19 @@
+#Method Of String
+a = 'abcd'
+b = 'abd123'
+c = '12345'
+d = 'ab cd'
+
+#CHECK ALPHA METHOD
+print(a.isalpha())
+print(b.isalpha())
+print(c.isalpha())
+#CHECK DIGIT METHOD
+print(a.isdigit())
+print(b.isdigit())
+print(c.isdigit())
+#CHECK BOTH APLHA AND DIGIT METHOD
+print(a.isalnum())
+print(b.isalnum())
+print(c.isalnum())
+print(d.isalnum())
