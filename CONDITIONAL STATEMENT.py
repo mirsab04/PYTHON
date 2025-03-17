e0f8cefc9f293ad8d63fc2915d776fe161ad152c
HAFIZ MUHAMMAD MIRSAB ALAM <mirsabalam989@gmail.com>
2025-03-17 13:07:35 +0500

    Add files via upload

---
+++ CONDITIONAL STATEMENT.py
@@ -0,0 +1,23 @@
+#IF Statement
+a = 5
+b = 3
+if a > b :
+    print("a is grater than b")
+
+#ELIF Statement
+c = int(input("Enter First Integer : "))
+d = int(input("Enter Second Integer : "))
+if c > d :
+    print("c is greater than d")
+elif c < d :
+    print("c is less than d")
+elif c == d :
+    print("c is equal to d")
+
+#Else Statement
+e = 5
+f = 3
+if e > f :
+    print("e is grater than f")
+else :
+    print("e is less than f")
